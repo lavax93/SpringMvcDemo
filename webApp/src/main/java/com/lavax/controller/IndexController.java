83c43c8b286cfac9f00ecab764a19514e02b4f95
LAvAX <yuanxin@nbeport.com>
2017-12-14 17:27:27 +0800

    增加log4j日志

--- webApp/src/main/java/com/lavax/controller/IndexController.java
+++ webApp/src/main/java/com/lavax/controller/IndexController.java
@@ -3,15 +3,21 @@ package com.lavax.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * Created by LAVAX on 2017/12/14.
  */
 @Controller
 public class IndexController {
+//    private static final Logger logger = LoggerFactory.getLogger(LoggerNames.LOGISTICS_COMPONENT);
+
     private static final String LIST_PAGE = "index";
 
     @RequestMapping("/index")
     public String toIndex() {
+//        logger.info("日志打印");
         return "/index";
     }
 }
